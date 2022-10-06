acd67b1318df0fcf5f32381f82b7d088f8472a19
Fagner Santiago <fagner.santiago@hotmail.com>
2022-10-06 15:21:48 -0300

    ajustes

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ProdutosModel } from './produtos.models';
 import { ProdutoService } from './produtos.services';
 
